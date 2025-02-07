e9b3a9f8f6d57f22102b5e116686e6099ea15607
imranBappy <imranbappy.official.com>
2025-02-07 22:58:05 +0600

    test deploy

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Pulling the latest changes..."
+git stash
+git pull origin master
+
+echo "Activating virtual environment..."
+source env/bin/activate
+
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "Applying migrations..."
+python3 manage.py migrate
+
+# echo "Collecting static files..."
+# python3 manage.py collectstatic --noinput
+
+echo "Restarting Gunicorn..."
+pkill gunicorn
+nohup gunicorn --bind 0.0.0.0:8000 my_tennis_club.wsgi:application > gunicorn.log 2>&1 &
+
+
+echo "Deployment completed!"

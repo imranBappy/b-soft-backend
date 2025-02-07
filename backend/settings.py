aaf450ad582555292145d339be235877294f1885
imranBappy <imranbappy.official.com>
2025-02-08 01:16:16 +0600

    test

--- backend/settings.py
+++ backend/settings.py
@@ -190,7 +190,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static') # Absolute path to where collectstatic will gather the static files
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
